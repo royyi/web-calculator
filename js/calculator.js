17f129e4a581646d1adccda488d1ba45c996aeaf
Roy Yi <ryi@ucdavis.edu>
2021-06-12 12:49:11 -0700

    cleaned code

--- js/calculator.js
+++ js/calculator.js
@@ -4,13 +4,7 @@
 
 var res = document.getElementById("res");
 
-// function append_char(e) {
-//     var btn = e.target;
-//     var char = document.getElementById(btn.id).innerHTML;
-//     res.innerHTML = res.innerHTML + char;
-// }
-
-// append character for each operator or operand
+// append character for each operator or operand, using event delegation
 const parent = document.getElementById("btns");
 
 parent.addEventListener("click", event => {
@@ -20,12 +14,7 @@ parent.addEventListener("click", event => {
     }
 });
 
-
-// var values = ["btn0", "btn1", "btnSum","btnSub", "btnMul", "btnDiv"];
-// for (let i in values) {
-//     document.getElementById(values[i]).addEventListener("click", append_char);
-// }
-
+// event listeners for clear and equal buttons
 var clear_btn = document.getElementById("clr_btn");
 clear_btn.addEventListener("click", function() {
     res.innerHTML = "";
@@ -33,13 +22,12 @@ clear_btn.addEventListener("click", function() {
 
 var eval_btn = document.getElementById("eql_btn");
 eval_btn.addEventListener("click", function() {
-    // var text = res.innerHTML;
-    
     res.innerHTML = Math.floor(eval(res.innerHTML));
-    // get position of operator
-    // var i = text.search(/[\+\-\*\/]/)
-    // var exp = parseInt(text.slice(0, i), 2) + text[i] + parseInt(text.slice(i+1), 2);
-    
-    // set result of evaluated expression
-    // res.innerHTML = Math.floor(eval(exp)).toString(2);
 })
+
+
+// old way: add event listener for each element of class
+// var values = ["btn0", "btn1", "btnSum","btnSub", "btnMul", "btnDiv"];
+// for (let i in values) {
+//     document.getElementById(values[i]).addEventListener("click", append_char);
+// }
